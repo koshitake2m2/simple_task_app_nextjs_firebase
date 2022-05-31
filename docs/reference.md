c5f3435203860e274a8a8f1ef2ab9bff44115b80
koshitake2m2 <blueling2m2dvp@gmail.com>
2022-05-31 19:35:50 +0900

    add mui

---
+++ docs/reference.md
@@ -0,0 +1,11 @@
+# 参考文献
+
+- 設計
+  - ディレクトリ構成
+    - [猫好きアプリケーション開発者記録 | Next.jsにおけるアーキテクチャー設計を考える](https://kkoudev.netlify.app/2020/05/nextjs-architecture-design/)
+- mui
+  - next.jsでmuiを利用するための設定(_app.tsx, _document.tsx, createEmotionCache.ts, theme.ts)
+    - [material-ui/examples/nextjs-with-typescript at master · mui/material-ui](https://github.com/mui/material-ui/tree/master/examples/nextjs-with-typescript)
+  - カスタムLink
+    - [Routing libraries - Material UI](https://mui.com/material-ui/guides/routing/#next-js)
+    - [material-ui/examples/nextjs-with-typescript at master · mui/material-ui](https://github.com/mui/material-ui/tree/master/examples/nextjs-with-typescript)
---
+++ src/pages/_app.tsx
@@ -0,0 +1,34 @@
+/**
+ * reference: https://github.com/mui/material-ui/blob/master/examples/nextjs-with-typescript/pages/_document.tsx
+ */
+import * as React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "ui/mui/theme";
+import createEmotionCache from "ui/mui/createEmotionCache";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
